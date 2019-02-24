f0bbce7d4bde94d555ce737c074132a0f9b3499a
Erfan Abdi <erfangplus@gmail.com>
2019-02-24 12:24:19 +0100

    cedric: Import libbinder and libcutils from griffin Needed by some blobs (camera and QSEE daemons)
    original commit:
    griffin: Don't bindmount libbinder and libcutils
    https://github.com/erfanoabdi/proprietary_vendor_motorola/commit/2e3a2588cd577e4c9dd753c04a2597c2ab4a392f
    
    Change-Id: I5d1f1ccee14c45c7c7d6c09a84ae1e8506dff2fd
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Change-Id: Iafe9824c061d6b4f3c5c0eee4eefa5b504cfaa3c

--- cedric-vendor.mk
+++ cedric-vendor.mk
@@ -1051,6 +1051,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/cedric/proprietary/vendor/lib/vendor.qti.hardware.perf@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.perf@1.0_vendor.so \
     vendor/motorola/cedric/proprietary/vendor/lib/vendor.qti.hardware.qdutils_disp@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.qdutils_disp@1.0.so \
     vendor/motorola/cedric/proprietary/vendor/lib/vendor.qti.hardware.radio.atcmdfwd@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.radio.atcmdfwd@1.0_vendor.so \
+    vendor/motorola/cedric/proprietary/vendor/lib/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/vndk-sp/libcutils.so \
+    vendor/motorola/cedric/proprietary/vendor/lib/vndk/libbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib/vndk/libbinder.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gatekeeper@1.0-impl-qti.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/com.qualcomm.qti.bluetooth_audio@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/com.qualcomm.qti.bluetooth_audio@1.0.so \
     vendor/motorola/cedric/proprietary/vendor/lib64/com.qualcomm.qti.imscmservice@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/com.qualcomm.qti.imscmservice@1.0_vendor.so \
